f50f00a09aed729b85e2362af22ecba65bb3d824
DaKheera47 <shaheergoodboy@gmail.com>
2026-01-21 19:28:03 +0000

    resolving dependencies correctly

--- orchestrator/vite.config.ts
+++ orchestrator/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@client': path.resolve(__dirname, './src/client'),
+      '@server': path.resolve(__dirname, './src/server'),
       '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
